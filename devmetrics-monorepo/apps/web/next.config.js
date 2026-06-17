a7f07086deaf56fabd858561b0649eb29e815fc1
Nathan <nathan@example.com>
2026-06-17 21:20:33 +0530

    fix bug

--- devmetrics-monorepo/apps/web/next.config.js
+++ devmetrics-monorepo/apps/web/next.config.js
@@ -1,12 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/ui"],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
